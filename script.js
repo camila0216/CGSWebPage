28d33173a44e6bc98808e1606ace8b7e85aa11de
Camila Gómez Schrader <c.gomezs@uniandes.edu.co>
2019-08-19 09:43:16 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+//Function that helps to update my bachelor studies once I graduate from university
+function gradDate(){
+  //Get's today's date
+  let today = new Date();
+  
+  //Variable for the month of the date.
+  let mm = today.getMonth() + 1; 
+  //Variable for the year of the date
+  let yyyy = today.getFullYear();
+
+  //Compares year and month to April of 2021 (Expected graduation date). I'm sure I will graduate in this date.
+  if(yyyy>=2021 && mm>=4)
+  {
+    document.getElementById("grado1").innerHTML = "Universidad de los Andes, Bogotá, Colombia. Industrial Engineering, August 2015 - December 2020, Bachelor degree in April, 2021.";
+    document.getElementById("grado2").innerHTML = "Universidad de los Andes, Bogotá, Colombia. Computer Science, August 2015 - December 2020, Bachelor degree in April, 2021.";
+  }
+}
